6def663cc68562b2dd3d17b0e1cade3b5086b91c
harshit-devops-2311 <harshitsahu0133@gmail.com>
2026-03-26 12:01:49 +0000

    added the day01

---
+++ day01/hello.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Harshit: This is my first script from harshit"
+echo "Student: what we will do next"
+echo "Harshit: We will do everything in shell"
+
+# this is my first comment
+
+<< comment
+anything written here is my comment
+line2
+line3
+line4
+comment
